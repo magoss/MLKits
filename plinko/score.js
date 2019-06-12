98b56025cee20b577ab46f3412a57f1838f7efbe
Marcin Gościniak <marcingosciniak@poczta.fm>
2019-06-12 13:38:32 +0200

    KNN implementation + splitting data into training nad test set

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,42 @@
+const output = [];
+const k = 100;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  output.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const [testSet, trainingSet] = splitDataset(output, 10);
+  testSet.forEach(item => {
+    const bucket = knn(trainingSet, item[0]);
+    console.log(bucket, item[3]);
+  });
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(point, predictionPoint) {
+	return Math.abs(point - predictionPoint);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
 
